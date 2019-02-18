a9e639e2cadd00c96d19b8e9847ac6226d6cff0d
locemarn <marcelon27@gmail.com>
2019-02-17 21:01:48 -0300

    contactmanager

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <div className="display-4">About Contact Manager</div>
+      <p className="lead">Simple app to manage contacts</p>
+      <p className="text-secondary">Version 1.0.0</p>
+    </div>
+  )
+}
+
+export default About
